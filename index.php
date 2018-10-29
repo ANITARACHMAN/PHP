e1e2e2d863b36ae866906fd7cc742965254e8606
arac <rachmananita12@gmail.com>
2018-10-29 09:07:53 +0700

    delete better

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <html>
 <head>
   <title>Menampilkan data dari database ke dalam bentuk tabel</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
   <div class="container">
     <h2><center>DATA PESERTA</center></h2>
-    <a href="input.php">INPUT DATA</a>
-    <table class="tabel" align="center">
+    <table class=" table table-bordered table-hovered" align="center">
   <tr>
-    <th>Id</th>
-    <th>Username</th>
-    <th>Password</th>
-    <th>Level</th>
-    <th>Fullname</th>
-    <th>Aksi</th>
-  </tr></div>
+    <th>ID</th>
+    <th>USERNAME</th>
+    <th>PASSWORD</th>
+    <th>LEVEL</th>
+    <th>FULLNAME</th>
+    <th>AKSI</th>
+  </tr>
+</div>
   <?php
- 
   include('koneksi.php');
-
-  $sql_tampil ="SELECT * FROM userrr order by id";
-
-  
+  $sql_tampil ="SELECT * FROM userrr  order by id";
   $peserta=mysqli_query($conn,$sql_tampil);
-
- 
   while($baris_data=mysqli_fetch_array($peserta,MYSQLI_ASSOC)){
-    ?>
+ ?>
     <tr>
       <td><?php echo $baris_data['id']; ?></td>
       <td><?php echo $baris_data['username']; ?></td>
@@ -36,10 +30,12 @@
       <td><?php echo $baris_data['level']; ?></td>
       <td><?php echo $baris_data['fullname']; ?></td>
       <td>
-      <a href="edit.php?id=<?php echo $baris_data['id']; ?>">Edit</a> |
-      <a href="delete.php?id=<?php echo $baris_data['id']; ?>">Hapus</a>
+      <a class="edit" href="edit.php?id=<?php echo $baris_data['id']; ?>">Edit</a> 
+      <a class="hapus" href="delete.php?id=<?php echo $baris_data['id']; ?>">Hapus</a>
+      <a href="input.php">Input</a>
       </td>
     </tr>
+    
 <?php } ?>
 </body>
 </html>
